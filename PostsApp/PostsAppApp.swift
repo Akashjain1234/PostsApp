0aa702a7e66daa3339fc27696493a25ddef5acec
akashjainleadtech <akash.jain@ext.leadtech.com>
2025-10-18 17:03:58 +0530

    Initial Commit

---
+++ PostsApp/PostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppApp.swift
+//  PostsApp
+//
+//  Created by Akash Jain on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
